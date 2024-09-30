764a87f1cb69ea878c57bb6cf152ed262109c3b5
jonkolong <jonkolong@gmail.com>
2024-09-30 14:53:16 +0700

    launcher pemkab ds

--- partials/scripts2.php
+++
@@ -1,9 +0,0 @@
-<!-- Import Js Files -->
-<script src="assets/libs/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
-<script src="assets/libs/simplebar/dist/simplebar.min.js"></script>
-<script src="assets/js/theme/app.init.js"></script>
-<script src="assets/js/theme/theme.js"></script>
-<script src="assets/js/theme/app.min.js"></script>
-
-<!-- solar icons -->
-<script src="https://cdn.jsdelivr.net/npm/iconify-icon@1.0.8/dist/iconify-icon.min.js"></script>
